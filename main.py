929faf53bf83127dcb969be9a2c703ae5ed866d3
loung12 <stepanenkoewan@yandex.ru>
2023-11-15 14:11:51 +0300

    upd

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 import random
-
-
-def print_board(board):
-    for i in range(9):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - - -")
-        for j in range(9):
-            if j % 3 == 0 and j != 0:
-                print("|", end=" ")
-            if j == 8:
-                print(board[i][j])
-            else:
-                print(str(board[i][j]) + " ", end=" ")
-
+from colorama import Fore
 
 def is_valid_move(board, row, col, num):
 
@@ -28,13 +15,11 @@ def is_valid_move(board, row, col, num):
 
     return True
 
-
 def is_board_full(board):
     for row in board:
         if 0 in row:
             return False
     return True
-
 
 def solve_board(board):
     for row in range(9):
@@ -49,9 +34,7 @@ def solve_board(board):
                 return False
     return True
 
-
 def generate_board():
-
     board = [[0] * 9 for _ in range(9)]
     solve_board(board)
 
@@ -63,6 +46,50 @@ def generate_board():
 
     return board
 
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - - -")
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end=" ")
+
+def is_valid_move(board, row, col, num):
+
+    for i in range(9):
+        if board[row][i] == num or board[i][col] == num:
+            return False
+
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+def is_board_full(board):
+    for row in board:
+        if 0 in row:
+            return False
+    return True
+
+def solve_board(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid_move(board, row, col, num):
+                        board[row][col] = num
+                        if solve_board(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
 
 def main():
     sudoku_board = generate_board()
@@ -71,22 +98,22 @@ def main():
         print_board(sudoku_board)
 
         try:
-            row = int(input("Введите номер строки (1-9): ")) - 1
-            col = int(input("Введите номер столбца (1-9): ")) - 1
-            num = int(input("Введите число (1-9): "))
+            row = int(input(Fore.GREEN + "Введите номер строки (1-9): ")) - 1
+            col = int(input(Fore.GREEN + "Введите номер столбца (1-9): ")) - 1
+            num = int(input(Fore.GREEN + "Введите число (1-9): "))
         except ValueError:
-            print("Некорректный ввод. Попробуйте снова.")
+            print(Fore.RED + "Некорректный ввод. Попробуйте снова.")
             continue
 
         if 0 <= row < 9 and 0 <= col < 9 and 1 <= num <= 9:
             if is_valid_move(sudoku_board, row, col, num):
                 sudoku_board[row][col] = num
             else:
-                print("Некорректное число. Попробуйте снова.")
+                print(Fore.RED + "Некорректное число. Попробуйте снова.")
         else:
-            print("Некорректные координаты. Попробуйте снова.")
+            print(Fore.RED + "Некорректные координаты. Попробуйте снова.")
 
-    print("Поздравляем! Вы решили головоломку!")
+    print(Fore.YELLOW + "Поздравляем! Вы решили головоломку!")
     print_board(sudoku_board)
 
 
